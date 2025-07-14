45609bb11931e2aefdf2e451e9867ff1c24941b5
maxcaut <maxcaut@yahoo.it>
2025-07-14 21:57:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ const username = document.querySelector('#username');
 const password =document.querySelector('#password');
 const accedi = document.querySelector('#Accedi-btn');
 
+const userSite = 'aibmassa'
+const passSite = 'AIB789'
+
 
 accedi.addEventListener('click', function(){
 
   
 
 
-  if (username.value === "111" && password.value === "111") {
+  if (username.value === userSite && password.value === passSite) {
     sessionStorage.setItem('stats', 1);
     window.location.href = '/dash.html';
   
   }
   else{
+    alert("Username o Password Errati");
     location.reload();
     
   }
