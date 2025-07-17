099d52a50b9ed1f50f320f2c160e334e63808a5c
maxcaut <maxcaut@yahoo.it>
2025-07-17 16:39:33 +0200

    Add files via upload

--- script-dash.js
+++ script-dash.js
@@ -10,3 +10,15 @@ window.location.href = '/index.html';
         
          
 setInterval(function() {location.reload();}, 120000); 
+
+
+
+const logOut = document.querySelector('#logout-btn');
+
+logOut.addEventListener('click', function(){
+    sessionStorage.setItem('stats', 0);
+    window.location.href = '/index.html';
+    
+    
+
+})
