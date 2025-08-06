6ee09c2aa885d0c7c9071e987f825e9f3378ff71
maxcaut <maxcaut@yahoo.it>
2025-08-06 07:18:32 +0200

    Update report-script.js

--- report-script.js
+++ report-script.js
@@ -31,13 +31,14 @@ function ritornaHome(){
 
 // Seleziona il form
 const form = document.querySelector("#report-form");
+const submitButton = document.getElementById("#bottoneInvia");
 
 // Aggiungi evento di submit
 form.addEventListener("submit", function(e) {
   e.preventDefault(); // evita refresh pagina
 
 // Disabilita il bottone submit
-  const submitButton = form.querySelector('button[type="submit"]');
+  
   submitButton.disabled = true;
   submitButton.textContent = "Invio in corso...";
 
